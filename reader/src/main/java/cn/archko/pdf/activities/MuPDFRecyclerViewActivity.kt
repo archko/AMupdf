939c3cc905ff1fabb66f4044fecc71db58cf40b4
archko <archko@gmail.com>
2020-07-23 19:00:58 +0800

    设置全屏

--- reader/src/main/java/cn/archko/pdf/activities/MuPDFRecyclerViewActivity.kt
+++ reader/src/main/java/cn/archko/pdf/activities/MuPDFRecyclerViewActivity.kt
@@ -170,6 +170,19 @@ abstract class MuPDFRecyclerViewActivity : AnalysticActivity() {
 
     open fun initView() {
         window.requestFeature(Window.FEATURE_NO_TITLE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val lp: WindowManager.LayoutParams = window.getAttributes()
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            window.attributes = lp
+        }
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                //View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                //View.SYSTEM_UI_FLAG_IMMERSIVE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         setContentView(R.layout.reader)
     }
 
