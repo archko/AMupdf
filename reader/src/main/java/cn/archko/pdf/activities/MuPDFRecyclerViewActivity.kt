b1e250b8e7303c057a45a44452037fa00aa96e65
archko <archko@gmail.com>
2020-07-28 09:26:26 +0800

    update lib add SYSTEM_UI_FLAG_IMMERSIVE|SYSTEM_UI_FLAG_IMMERSIVE_STICKY release 4.6.1

--- reader/src/main/java/cn/archko/pdf/activities/MuPDFRecyclerViewActivity.kt
+++ reader/src/main/java/cn/archko/pdf/activities/MuPDFRecyclerViewActivity.kt
@@ -175,12 +175,14 @@ abstract class MuPDFRecyclerViewActivity : AnalysticActivity() {
             lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
             window.attributes = lp
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+        }
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
                 View.SYSTEM_UI_FLAG_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                //View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                //View.SYSTEM_UI_FLAG_IMMERSIVE or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         setContentView(R.layout.reader)
